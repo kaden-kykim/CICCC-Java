cb8169c07a6e97770dc7253d41bdec74eae8f00c
kaden-kykim <kaden.kykim@gmail.com>
2019-11-23 18:47:01 -0800

    Initialize Java Project structure

---
+++ Project_Final/README.md
@@ -0,0 +1,3 @@
+# Final Project: P2P File Sharing (Temp)
+Final Project for Java Class.<br>
+Temporarily, the name of this project is P2P File sharing
---
+++ Project_Final/src/ca/ciccc/wmad/kaden/java_final/P2PFileSharing.java
@@ -0,0 +1,8 @@
+package ca.ciccc.wmad.kaden.java_final;
+
+public class P2PFileSharing {
+
+    public static void main(String[] args) {
+        System.out.println("Initialize Java Project");
+    }
+}
