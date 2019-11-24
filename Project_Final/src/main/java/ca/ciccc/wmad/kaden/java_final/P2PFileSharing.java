233133ee13142f5f64e5d61f5d20fdfdf00b7dec
kaden-kykim <kaden.kykim@gmail.com>
2019-11-23 21:45:33 -0800

    Maven test with Gson

--- Project_Final/src/main/java/ca/ciccc/wmad/kaden/java_final/P2PFileSharing.java
+++ Project_Final/src/main/java/ca/ciccc/wmad/kaden/java_final/P2PFileSharing.java
@@ -1,8 +1,12 @@
 package ca.ciccc.wmad.kaden.java_final;
 
+import com.google.gson.Gson;
+
 public class P2PFileSharing {
 
     public static void main(String[] args) {
         System.out.println("Initialize Java Project");
+        Gson gson = new Gson();
+        System.out.println("Load Gson");
     }
 }
